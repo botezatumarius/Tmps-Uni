16b8e661bb43a6709f5f5d0d566fcc1ad61dc356
botezatumarius <ufobelievers27@gmail.com>
2023-10-09 16:15:24 +0300

    Started lab2

--- Lab 2 - creational dp/src/Main.java
+++ Lab 2 - creational dp/src/Main.java
@@ -1,8 +1,3 @@
-import domain.airplanes.BoeingBuilder;
-import domain.airports.General;
-import domain.availability.AddPassenger;
-import domain.availability.AddPlane;
-import domain.passengers.Adult;
 
 public class Main {
     public static void main(String[] args) throws Exception {
